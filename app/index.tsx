db23a2f5fcf832811e720afb5439fb8fb72c387d
Nana Mohamadi <adilnana78@gmail.com>
2025-07-05 08:52:10 +0000

    NativeWind Setup

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-5xl font-bold text-center text-primary font-quicksand-semibold">
+        Welcome to my App Food Delivery!
+      </Text>
     </View>
   );
 }
