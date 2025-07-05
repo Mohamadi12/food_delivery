f6399813840f862886b1e37115b2b21e11d8ae69
Nana Mohamadi <adilnana78@gmail.com>
2025-07-05 21:37:28 +0000

    Auth Screens

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,6 +1,8 @@
 export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-     platform: "com.nana.food_delivery"
+     platform: "com.nana.food_delivery",
+     databaseId: '686968f90024870ef066',
+     userCollectionId: '68696b41000375d06d53'
 }
 
